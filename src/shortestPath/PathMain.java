ffa6490e4ce7842d938fea7a95acbee180e74451
Cbayy <53809379+Cbayy@users.noreply.github.com>
2021-07-11 22:49:45 +0930

    Added to main the ability for user input to add vertices and edges to create a graph.

--- src/shortestPath/PathMain.java
+++ src/shortestPath/PathMain.java
@@ -1,5 +1,7 @@
 package shortestPath;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PathMain {
@@ -13,9 +15,29 @@ public class PathMain {
 
         int test = Integer.parseInt(line[0]);
          */
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> input = new ArrayList<String>();
 
+        while(true){
+            String text = in.nextLine();
+            if(text.equals("stop")){
+                break;
+            }
+            input.add(text);
+        }
         Graph g = new Graph();
-        g.adjMatrix = new int[6][6];
+        g.adjMatrix = new int[input.size()][input.size()];
+
+        for(String s: input){
+            String[] line = s.split(" ");
+            int v = Integer.parseInt(line[0]);
+            int w = Integer.parseInt(line[1]);
+            int d = Integer.parseInt(line[2]);
+            g.addEdge(v,w,d);
+        }
+
+
+        /*
 
         g.addEdge(0,1,18);
         g.addEdge(0,2,12);
@@ -26,7 +48,7 @@ public class PathMain {
         g.addEdge(3,0,31);
         g.addEdge(4,0,31);
         g.addEdge(0,5,2);
-
+        */
 
         g.shortestPath(0);
 
