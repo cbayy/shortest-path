5887bc06f0863661d285dbb6421daa12b3fa1e7a
Cbayy <53809379+Cbayy@users.noreply.github.com>
2023-01-23 23:07:36 +1100

    Refactored paintComponent, starting to add get node from click.

--- src/shortestPath/GraphPanel.java
+++ src/shortestPath/GraphPanel.java
@@ -2,6 +2,9 @@ package shortestPath;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Random;
 
 public class GraphPanel extends JPanel {
@@ -11,8 +14,16 @@ public class GraphPanel extends JPanel {
     int edgePos[][];
     boolean painted = false;
 
-    public GraphPanel(){
+    private int selectedNode = 0;
 
+    public GraphPanel(){
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                System.out.println("Coordinates: " + e.getX() + " : " + e.getY());
+            }
+        });
     }
 
     public void setAdjMatrix(int[][] adjMatrixNew){
@@ -26,39 +37,45 @@ public class GraphPanel extends JPanel {
         Graphics2D g2D = (Graphics2D) g;
         super.paintComponent(g2D);
         if (adjMatrix != null) {
+            paintNodes(g, g2D);
+            paintEdges(g, g2D);
+        }
+    }
 
-            //Loops through the adjacency matrix, calculates positions on the frame for the nodes of the graph and paints them to screen
-            for (int i = 0; i < adjMatrix.length; i++) {
-                g.setColor(Color.darkGray);
-                int xPos = 0;
-                int yPos = 0;
-                if(painted==true) {
-                    Random rand = new Random();
-                    xPos = rand.nextInt(800);
-                    yPos = rand.nextInt(600);
-                }else{
-                    xPos = edgePos[i][0];
-                    yPos = edgePos[i][1];
-                }
-                g2D.fillOval(xPos, yPos, 50, 50);
-                g.setColor(Color.white);
-                g2D.drawString(String.valueOf(i), xPos + 20, yPos + 20);
-                edgePos[i][0] = xPos;
-                edgePos[i][1] = yPos;
+    private void paintNodes(Graphics g, Graphics2D g2D){
+        //Loops through the adjacency matrix, calculates positions on the frame for the nodes of the graph and paints them to screen
+        for (int i = 0; i < adjMatrix.length; i++) {
+            g.setColor(Color.darkGray);
+            int xPos = 0;
+            int yPos = 0;
+            if(painted==true) {
+                Random rand = new Random();
+                xPos = rand.nextInt(800);
+                yPos = rand.nextInt(600);
+            }else{
+                xPos = edgePos[i][0];
+                yPos = edgePos[i][1];
             }
+            g2D.fillOval(xPos, yPos, 50, 50);
 
-            //Loops through the adjacency matrix and paints each edge and the weight of each edge
-            painted = true;
-            for (int i = 0; i < adjMatrix.length; i++) {
-                for (int j = 0; j < adjMatrix.length; j++) {
-                    if(adjMatrix[i][j] > 0) {
-                        g.setColor(Color.red);
-                        g2D.drawLine(edgePos[i][0] + 25, edgePos[i][1] + 25, edgePos[j][0] + 25, edgePos[j][1] + 25);
-                        g2D.drawString(String.valueOf(adjMatrix[i][j]),(edgePos[i][0] + edgePos[j][0])/2 + 25, (edgePos[i][1] + edgePos[j][1])/2 +25);
-                    }
+            g.setColor(Color.white);
+            g2D.drawString(String.valueOf(i), xPos + 20, yPos + 20);
+            edgePos[i][0] = xPos;
+            edgePos[i][1] = yPos;
+        }
+    }
+
+    private void paintEdges(Graphics g, Graphics2D g2D){
+        //Loops through the adjacency matrix and paints each edge and the weight of each edge
+        painted = true;
+        for (int i = 0; i < adjMatrix.length; i++) {
+            for (int j = 0; j < adjMatrix.length; j++) {
+                if(adjMatrix[i][j] > 0) {
+                    g.setColor(Color.red);
+                    g2D.drawLine(edgePos[i][0] + 25, edgePos[i][1] + 25, edgePos[j][0] + 25, edgePos[j][1] + 25);
+                    g2D.drawString(String.valueOf(adjMatrix[i][j]),(edgePos[i][0] + edgePos[j][0])/2 + 25, (edgePos[i][1] + edgePos[j][1])/2 +25);
                 }
             }
-
         }
     }
 }
